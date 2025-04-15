d77f0b92d0fb533f33fe41ae5f5206ab996b7f16
Gabriel Vicent <rodriguezisaac876@gmail.com>
2025-04-15 17:51:44 -0400

    PRIMER COMMIT , ADD .GITIGNORE

---
+++ src/main/java/devforge/susuManager/SusuManagerApplication.java
@@ -0,0 +1,13 @@
+package devforge.susuManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusuManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusuManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devforge/susuManager/SusuManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package devforge.susuManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusuManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
